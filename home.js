427c2f9c537ccaee8d2475427e828137d04ba8e8
Deepkamal yadav <dkits1995@gmail.com>
2022-06-16 11:23:27 +0530

    modification

---
+++ home.js
@@ -0,0 +1,360 @@
+
+    let homedata = [
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/d9aa6a06-225f-4a82-862c-f31c07c796df.jpeg?h=365&w=240&dpr=2",
+      name: "Marta B. Camarasa Abstract Side Table",
+      price: 13753,
+      strikedoffprice: "$2599",
+      productid:1,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/7715c926-f093-4994-9f25-790aa9e08ae1.jpeg?h=365&w=240&dpr=2",
+      name: "Round Tray Base Stand",
+      price: 15004,
+      strikedoffprice: 999,
+      productid:2,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/ffe82656-2074-48c6-be2b-71748104fbb4.jpeg?h=365&w=240&dpr=2",
+      name: " Sven Counter Stool",
+      price:  16254,
+      strikedoffprice: 2099,
+      productid:3,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/c8904541-3d61-4bfe-908c-9c2b2fbf2321.jpeg?h=365&w=240&dpr=2",
+      name: "Echo Number Three Wall Clock",
+      price: 6251,
+      strikedoffprice: 799,
+      productid:4,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/c005cd1d-4036-4e8b-8990-dfc163ccf7f7.jpeg?h=365&w=240&dpr=2",
+      name: "'Pay Phone' Wall Phone",
+      price: 5834,
+      strikedoffprice: 3599,
+      productid:5,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/a3f47afb-d0d1-40d6-aa3b-bac3cbcb9651.jpeg?h=365&w=240&dpr=2",
+      name: " Maddox Wall Clock",
+      price: 6688,
+      strikedoffprice: 3199,
+      productid:6,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/35be92d8-4112-4d78-88a9-cfcbf37288ff.jpeg?h=365&w=240&dpr=2 ",
+      name: " Whistler Throw Blanket (Nordstrom Exclusive) ",
+      price: 8168,
+      strikedoffprice: 3199,
+      productid:7,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/09115973-7dc0-41ea-a228-c255dd35011d.jpeg?h=365&w=240&dpr=2",
+      name: "Sport Earbuds",
+      price: 769,
+      strikedoffprice: 1099,
+      productid:8,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/1a35d6fb-4173-4cc4-9995-6fd33530cf75.jpeg?h=365&w=240&dpr=2",
+    
+      name: "Compact 2-Quart Air Fryer",
+      price:5000,
+      strikedoffprice: 1440,
+      productid:9,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/57ea0e56-2c1f-4262-be4a-3c1d1491e72d.jpeg?h=365&w=240&dpr=2",
+      name: "Moccamaster KBGV Select Coffee Brewer",
+      price: 5099,
+      strikedoffprice: 999,
+      productid:10,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/96196e24-a95c-4891-a08c-1346754aa60c.jpeg?h=365&w=240&dpr=2",
+      name: "smeg 50s Retro Style Two-Slice Toaster ",
+      price: 2083,
+      strikedoffprice: 3599,
+      productid:11,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/616d3265-9e76-4d18-b7bb-37288f02be41.jpeg?h=365&w=240&dpr=2",
+      name: "JURA E6 Automatic Coffee Machine ",
+      price: 133286,
+      strikedoffprice: 3599,
+      productid:12,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/3bacfc21-e8d7-4aac-ad66-efedda37d06e.jpeg?h=365&w=240&dpr=2",
+      name: "Hydro Flask 32-Ounce Wide Mouth Cap Bottle ",
+      price:3746,
+      strikedoffprice: 999,
+      productid:13,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/8742e5dc-ed4e-4d56-a5bc-6b88f14caf0a.jpeg?h=365&w=240&dpr=2 ",
+      name: " Estelle Colored Glass Set of 2 Stem Wineglasses",
+      price:   7085.,
+      strikedoffprice: 3499,
+      productid:14,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/f32ba76d-f404-40a9-9cca-bc1061ae963d.jpeg?h=365&w=240&dpr=2",
+      name: "Nachtmann Assorted Set of 4 Crystal Double Old Fashioned Glasses ",
+      price: 2500,
+      strikedoffprice: 12000,
+      productid:15,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/60d10964-1804-4dea-9236-f6bac0f533f1.jpeg?h=365&w=240&dpr=2",
+      name: " Nordstrom Marble Serving Board",
+      price: 779,
+      strikedoffprice: 1299,
+      productid:16,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/2ac5d781-2558-4ea3-b817-1f433212f8a6.jpeg?h=365&w=240&dpr=2",
+      name: "Nambé Set of 4 Butterfly Salad Bowls",
+      price: 1019,
+      strikedoffprice: 1699,
+      productid:17,
+    },
+    {
+      image_url:
+        "https://n.nordstrommedia.com/id/sr3/0dc28be7-eab9-4bc9-a983-95e25a6c0f6a.jpeg?h=365&w=240&dpr=2",
+      name: "Chilewich Quill Table Runner ",
+      price: 1019,
+      strikedoffprice: 1699,
+      productid:18,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-FW0318/reebok_FW0318_1.jpg.plp ",
+      name: " MEN'S REEBOK RUNNING TREND SHOES",
+      price: 1575,
+      strikedoffprice: 3499,
+      productid:19,
+    },
+    {
+      image_url:
+        " https://content.shop4reebok.com/static/Product-G57776/reebok_G57776_1.jpg.plp ",
+      name: "  MEN'S REEBOK RUNNING ZTAUR RUN SHOES ",
+      price: 3499,
+      strikedoffprice: 4999,
+      productid:20,
+    },
+    {
+      image_url:
+        " https://content.shop4reebok.com/static/Product-FV9555/MEN_SWIM_SLIPPERS_FV9555_1.jpg.plp  ",
+      name: "MEN'S REEBOK SWIM ULTRA FLIP SLIPPERS   ",
+      price: 749,
+      strikedoffprice: 1499,
+      productid:21,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-EX3994/reebok_EX3994_1.jpg.plp   ",
+      name: "  MEN'S REEBOK SWIM FLEX CONNECT SANDALS ",
+      price: 1149,
+      strikedoffprice: 2299,
+      productid:22,
+    },
+    {
+      image_url:
+        " https://content.shop4reebok.com/static/Product-EX4287/reebok_EX4287_1.jpg.plp  ",
+      name: " MEN'S REEBOK RUNNING GENESIS RUNNER SHOE  ",
+      price: 1620,
+      strikedoffprice: 3599,
+      productid:23,
+    },
+    {
+      image_url:
+        " https://content.shop4reebok.com/static/Product-GK9180/reebok_GK9180_1.jpg.plp  ",
+      name: " MEN'S REEBOK TRAINING RUNNER SHORTS  ",
+      price: 779,
+      strikedoffprice: 1299,
+      productid:24,
+    },
+    {
+      image_url:
+        " https://content.shop4reebok.com/static/Product-HB5732/reebok_HB5732_1.jpg.plp  ",
+      name: " MEN REEBOK CLASSICS TRACKSUIT HOODIE  ",
+      price: 3499,
+      strikedoffprice: 4999,
+      productid:25,
+    },
+    {
+      image_url:
+        " https://content.shop4reebok.com/static/Product-HB5733/reebok_HB5733_1.jpg.plp  ",
+      name: "MEN'S REEBOK CLASSIC TRACKSUIT GOLF HOODIE   ",
+      price: 3499,
+      strikedoffprice: 4999,
+      productid:26,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-FQ5364/reebok_FQ5364_1.jpg.plp   ",
+      name: " UNISEX REEBOK TRAINING ACTIVE  BAG ",
+      price: 1399,
+      strikedoffprice: 2799,
+      productid:27,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-FW0318/reebok_FW0318_1.jpg.plp ",
+      name: " MEN'S REEBOK RUNNING TREND RUNNER SHOES",
+      price: 1575,
+      strikedoffprice: 3499,
+      productid:28,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-G57705/reebok_G57705_1.jpg.plp",
+      name: "UNISEX REEBOK CLASSICS  BASKETBALL SHOES",
+      price: 3919,
+      strikedoffprice: 5599,
+      productid:29,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-FW0344/reebok_FW0344_1.jpg.plp",
+      name: "MEN'S REEBOK RUNNING RUN FUSION  SHOES",
+      price: 1620,
+      strikedoffprice: 3599,
+      productid:30,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-EX4272/reebok_EX4272_1.jpg.plp",
+      name: "MEN'S REEBOK RUNNING QUICK DRIFTER SHOES",
+      price: 1620,
+      strikedoffprice: 3599,
+      productid:31,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-H02293/reebok_H02293_1.jpg.plp",
+      name: "MEN'S REEBOK DYNAMIC ZIGZAG SHOES",
+      price: 7599,
+      strikedoffprice: 9999,
+      productid:32,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-FV9551/MEN_SWIM_SLIPPERS_FV9551_1.jpg.plp",
+      name: "MEN'S REEBOK SWIM SUPERSOFT FLIP SLIPPERS",
+      price: 909,
+      strikedoffprice: 1299,
+      productid:33,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-HC3528/reebok_HC3528_1.jpg.plp",
+      name: "MEN'S REEBOK TRAINING ACTIVE BASIC POLO TEE",
+      price: 779,
+      strikedoffprice: 1299,
+      productid:34,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-HF3794/reebok_HF3794_01.JPG.plp ",
+      name: "MEN'S REEBOK TRAINING ACTIVE MOVE TEE ",
+      price: 1749,
+      strikedoffprice: 2499,
+      productid:35,
+    },
+    {
+      image_url:
+        "https://content.shop4reebok.com/static/Product-HG5456/reebok_HG5456_1.jpg.plp",
+      name: "MEN'S REEBOK FITNESS TRAINING  SWEATSHIRT",
+      price: 1079,
+      strikedoffprice: 1799,
+      productid:36,
+    },
+  ];
+
+let cartLs=  JSON.parse(localStorage.getItem("cart_page")) || [];
+
+
+function handlesalrysort(){
+    
+      let selected=document.querySelector("#sortsalary").value
+
+      if(selected=="Low to High"){
+         homedata.sort(function(a,b){
+           return a.price-b.price;
+         })
+         display(homedata);
+      }
+      if(selected=="High to low"){
+         homedata.sort(function(a,b){
+           return b.price-a.price
+         })
+         display(homedata);
+      }
+    }
+          
+
+
+       function display(homedata){
+        document.querySelector("#container").innerHTML="";
+        homedata.forEach(function(elem){
+            
+    let box=document.createElement("div")
+    let image=document.createElement("img")
+    image.setAttribute("src",elem.image_url)
+    let name=document.createElement("h4")
+    name.innerText=elem.name;
+    let price=document.createElement("p")
+    price.innerText=elem.price;
+    let button=document.createElement("button")
+    button.innerText="Add to cart"
+    button.addEventListener("click",function(){
+        if(addtocart(elem.productid)==true){
+            alert("Product added to Suessfully")
+            cartLs.push(elem);
+            localStorage.setItem("cart_page",JSON.stringify(cartLs));
+        }
+        else{
+            alert("Product allready in the cart")
+        }
+         addtocart(elem.productid);
+    })
+
+
+    box.append(image,name,price,button)
+    document.querySelector("#container").append(box)
+  })
+       }
+       
+     display(homedata);
+
+
+    function addtocart(id){
+     for(let i=0;i<cartLs.length;i++){
+     if(cartLs[i].productid==id){
+        return false;
+     }
+}
+return true;
+       }
+
+
