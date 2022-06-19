90c287bc0017b4b81c77aa0dca761887510b7159
Md-razvi <mraza1748@gmail.com>
2022-06-19 12:50:43 +0530

    MEns_pae

--- Mens.js
+++ Mens.js
@@ -2,91 +2,91 @@ let MensData=[
     {
         type:"pant",
         ProductID:"Men_p1234",
-        name:"Bonobos",
+        Brand_name:"Bonobos",
         image_url:"https://n.nordstrommedia.com/id/sr3/4414f97d-cadf-41a4-ba5b-9aa8e5a0a580.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",        
         price: "8252.26"
     },
     {
         type:"pant",
         ProductID:"Men_p678#1",
-        name:"vuori",
+        Brand_name:"vuori",
         image_url:"https://n.nordstrommedia.com/id/sr3/cea59391-4670-41e4-9f96-583266948809.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"7418.70"
     },
     {
         type:"pant",
         ProductID:"Men_p678#2",
-        name:"AG",
+        Brand_name:"AG",
         image_url:"https://n.nordstrommedia.com/id/sr3/0e7486a9-93bb-47d9-947c-5a4052ff4157.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"16504.53"
     },
     {
         type:"pant",
         ProductID:"Men_p678#3",
-        name:"Devil-Dog Dungarees",
+        Brand_name:"Devil-Dog Dungarees",
         image_url:"https://n.nordstrommedia.com/id/sr3/0c42f6d8-50b0-47d4-b057-330b2622f7f9.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",     
         price:"4938.85"
     },
     {
         type:"pant",
         ProductID:"Men_p678#4",
-        name:"Bonobos",
+        Brand_name:"Bonobos",
         image_url:"https://n.nordstrommedia.com/id/sr3/f553ea53-5fe2-4b95-917b-d1250db7c161.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"8168.91"
     },
     {
         type:"pant",
         ProductID:"Men_s678#6",
-        name:"vuori",
+        Brand_name:"vuori",
         image_url:"https://n.nordstrommedia.com/id/sr3/3eba4372-4be0-4864-80ef-7adb28bca83f.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"7001.92"
     },
     {
         type:"short",
         ProductID:"Men_s678#7",
-        name:"vuori",
+        Brand_name:"vuori",
         image_url:"https://n.nordstrommedia.com/id/sr3/937d5b3b-70ec-411c-b430-514b9dc8b1e1.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"5668.22"
     },
     {
         type:"short",
         ProductID:"Men_s678#8",
-        name:"Kore Shorts",
+        Brand_name:"Kore Shorts",
         image_url:"https://n.nordstrommedia.com/id/sr3/6e25ad32-57e1-4038-8d58-cc5e1d433bb3.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"5668.22"
     },
     {
         type:"short",
         ProductID:"Men_s678#9",
-        name:"Zella",
+        Brand_name:"Zella",
         image_url:"https://n.nordstrommedia.com/id/sr3/68f62117-fff3-4d2a-b0bd-f851fde947df.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"4918.02"
     },
     {
         type:"jeans",
         ProductID:"Men_j678#10",
-        name:"PAIGE",
+        Brand_name:"PAIGE",
         image_url:"https://n.nordstrommedia.com/id/sr3/f305d54f-b29d-44cd-b946-8667a0964ab6.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"15420.90"
     },
     {
         type:"jeans",
         ProductID:"Men_j678#11",
-        name:"PAIGE",
+        Brand_name:"PAIGE",
         image_url:"https://n.nordstrommedia.com/id/sr3/f305d54f-b29d-44cd-b946-8667a0964ab6.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"12420.90"
     },
     {
         type:"jeans",
         ProductID:"Men_j678#12",
-        name:"PURPLE BRAND",
+        Brand_name:"PURPLE BRAND",
         image_url:"https://n.nordstrommedia.com/id/sr3/bf62c12f-2694-476f-b2a2-f8b4a290ae40.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"15670.97"
     },
     {
         type:"jeans",
         ProductID:"Men_j678#13",
-        name:"PURPLE BRAND",
+        Brand_name:"PURPLE BRAND",
         image_url:"https://n.nordstrommedia.com/id/sr3/bf62c12f-2694-476f-b2a2-f8b4a290ae40.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"10670.97"
 
@@ -94,28 +94,28 @@ let MensData=[
     {
         type:"jeans",
         ProductID:"Men_j678#14",
-        name:"AG",
+        Brand_name:"AG",
         image_url:"https://n.nordstrommedia.com/id/sr3/c21b278c-c21d-4f3f-962c-96ffd679daba.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"12340.67"
     },
     {
         type:"shirt",
         ProductID:"Men_st678#15",
-        name:"ROBERT BARAKETT",
+        Brand_name:"ROBERT BARAKETT",
         image_url:"https://n.nordstrommedia.com/id/sr3/18edfd0b-1602-4909-9263-6ea261993b69.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"3670"
     },
     {
         type:"shirt",
         ProductID:"Men_st678#16",
-        name:"Zella",
+        Brand_name:"Zella",
         image_url:"https://n.nordstrommedia.com/id/sr3/c5851198-17e4-40c3-b8d3-68c0c27041c3.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"7444.60"
     },
     {
         type:"polo-shirt",
         ProductID:"Men_stp678#17",
-        name:"Matrix",
+        Brand_name:"Matrix",
         image_url:"https://n.nordstrommedia.com/id/sr3/cfe14caf-6c6e-4154-8687-c0b2cf757c4c.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"6750"
     }
@@ -123,7 +123,7 @@ let MensData=[
     {
         type:"polo-shirt",
         ProductID:"Men_stp678#18",
-        name:"Matrix",
+        Brand_name:"Matrix",
         image_url:"https://n.nordstrommedia.com/id/sr3/cfe14caf-6c6e-4154-8687-c0b2cf757c4c.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         price:"7750"
     }
@@ -201,7 +201,7 @@ function DisplayMensProduct(product_Data)
         let bt=document.createElement("button")
         bt.innerText="Add to Bag"
         p2.textContent="INR: "+el.price;
-        p1.textContent=el.name;
+        p1.textContent=el.Brand_name;
         image.src=el.image_url;
         bt.addEventListener("click",function()
         {
